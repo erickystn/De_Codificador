ded79419fede170f3de49166dc749c498ee46c8d
erickystn <xkyx@live.com>
2024-07-08 02:09:47 -0300

    fixed settimeout copy button

--- js/script.js
+++ js/script.js
@@ -1,87 +1,89 @@
 function encrypt(text) {
-    const keys = [
-        ["a", "ai"],
-        ["e", "enter"],
-        ["i", "imes"],
-        ["o", "ober"],
-        ["u", "ufat"],
-    ];
+  const keys = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
 
-    return text
-        .split("")
-        .map((item) =>
-            keys.reduce((prev, act) => (prev === act[0] ? act[1] : prev), item)
-        )
-        .join("");
+  return text
+    .split("")
+    .map((item) =>
+      keys.reduce((prev, act) => (prev === act[0] ? act[1] : prev), item)
+    )
+    .join("");
 }
 
 function decrypt(textEncrypt) {
-    const keys = [
-        ["a", "ai"],
-        ["e", "enter"],
-        ["i", "imes"],
-        ["o", "ober"],
-        ["u", "ufat"],
-    ];
-    return keys.reduce(
-        (prev, act) => prev.replaceAll(act[1], act[0]),
-        textEncrypt
-    );
+  const keys = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  return keys.reduce(
+    (prev, act) => prev.replaceAll(act[1], act[0]),
+    textEncrypt
+  );
 }
 
 function showResult(text) {
-    document
-        .querySelector(".second-section-not-found")
-        .classList.add("invisible");
+  document
+    .querySelector(".second-section-not-found")
+    .classList.add("invisible");
 
-    document
-        .querySelector(".second-section-result")
-        .classList.remove("invisible");
+  document
+    .querySelector(".second-section-result")
+    .classList.remove("invisible");
 
-    document.querySelector(".second-section-result__text").innerHTML = text;
+  document.querySelector(".second-section-result__text").innerHTML = text;
 }
 
 function resetResult() {
-    document
-        .querySelector(".second-section-not-found")
-        .classList.remove("invisible");
+  document
+    .querySelector(".second-section-not-found")
+    .classList.remove("invisible");
 
-    document.querySelector(".second-section-result").classList.add("invisible");
+  document.querySelector(".second-section-result").classList.add("invisible");
 
-    document.querySelector(".second-section-result__text").innerHTML = "";
+  document.querySelector(".second-section-result__text").innerHTML = "";
 }
 
 document.querySelector("#crypt").addEventListener("click", (_) => {
-    const text = document.querySelector("#text-input").value;
-    if (!text) {
-        resetResult();
-        return;
-    }
-    showResult(encrypt(text));
+  const text = document.querySelector("#text-input").value;
+  if (!text) {
+    resetResult();
+    return;
+  }
+  showResult(encrypt(text));
 });
 
 document.querySelector("#decrypt").addEventListener("click", (_) => {
-    const text = document.querySelector("#text-input").value;
-    if (!text) {
-        resetResult();
-        return;
-    }
-    showResult(decrypt(text));
+  const text = document.querySelector("#text-input").value;
+  if (!text) {
+    resetResult();
+    return;
+  }
+  showResult(decrypt(text));
 });
 document.querySelector("#copy").addEventListener("click", async (e) => {
-    try {
+  try {
+    const text = document.querySelector(
+      ".second-section-result__text"
+    ).textContent;
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+      await navigator.clipboard.writeText(text);
+      e.target.innerHTML = "Copiado!";
+      e.target.setAttribute("disabled", true);
 
-
-        const text = document.querySelector(".second-section-result__text").textContent;
-        if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
-            await navigator.clipboard.writeText(text);
-            e.target.innerHTML = "Copiado!"
-            e.target.setAttribute("disabled", true);
-
-            setTimeout(_ => { e.target.innerHTML = "Copiar", e.target.removeAttribute('disabled'); }, 2000);
-        }
-    } catch (ex) {
-        console.log("The Clipboard API is not available.", ex);
+      setTimeout((_) => {
+        e.target.innerHTML = "Copiar";
+        e.target.removeAttribute("disabled");
+      }, 2000);
     }
+  } catch (ex) {
+    console.log("The Clipboard API is not available.", ex);
+  }
 });
-
